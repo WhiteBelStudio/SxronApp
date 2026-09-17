76646dac9d9251303b1e17c2453ad1e7243b1863
WhiteBelStudio <nikitinka221@gmail.com>
2026-09-17 17:51:50 +0300

    chore(release): sync public API version to 1.1.19

--- server/public_api.py
+++ server/public_api.py
@@ -9,7 +9,7 @@ from pydantic import BaseModel
 import server.main as base
 
 app = base.app
-base.APP_VERSION = "1.1.17"
+base.APP_VERSION = "1.1.19"
 base.app.version = base.APP_VERSION
 init_db = base.init_db
 
