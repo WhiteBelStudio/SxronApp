e9bbebb0caf6e64b423792611105b6d9c63956d1
WhiteBelStudio <nikitinka221@gmail.com>
2026-09-17 17:01:37 +0300

    Move update controls into profile for v1.1.14

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import AuthGate from "./components/AuthGate";
 import AdminPanel from "./components/AdminPanel";
-import ForcedUpdater from "./components/ForcedUpdater";
 import PublicSellerProfile from "./components/PublicSellerProfile";
 
 import "./styles/global.css";
 import "./styles/user-profile.css";
 
-const APP_VERSION = "1.1.13";
+const APP_VERSION = "1.1.14";
 
 function syncStaticVersionLabels() {
   document.title = `SXRON Marketplace ${APP_VERSION}`;
@@ -30,7 +29,6 @@ createRoot(rootElement).render(
     <AuthGate>
       <App />
       <AdminPanel />
-      <ForcedUpdater />
       <PublicSellerProfile />
     </AuthGate>
   </StrictMode>,
