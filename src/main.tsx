59678aa992ae9eec58e7f861420d1ea0aa09e5ab
WhiteBelStudio <nikitinka221@gmail.com>
2026-09-18 22:38:14 +0300

    SXRON Marketplace 1.2.20
    Fix pydantic_core bundling and repair Windows install path.

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ import "./styles/user-profile.css";
 import "./styles/theme.css";
 import "./styles/desktop-overrides.css";
 
-const APP_VERSION = "1.2.19";
+const APP_VERSION = "1.2.20";
 
 function syncStaticVersionLabels() {
   document.title = `SXRON Marketplace ${APP_VERSION}`;
