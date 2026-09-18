5df037a09d0597adfcc2d51358bcbef1ac0d85c4
WhiteBelStudio <nikitinka221@gmail.com>
2026-09-18 21:49:31 +0300

    SXRON Marketplace 1.2.16
    Finalize PC desktop design and Windows installer configuration.

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ import "./styles/user-profile.css";
 import "./styles/theme.css";
 import "./styles/desktop-overrides.css";
 
-const APP_VERSION = "1.2.15";
+const APP_VERSION = "1.2.16";
 
 function syncStaticVersionLabels() {
   document.title = `SXRON Marketplace ${APP_VERSION}`;
