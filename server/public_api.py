739e82a953adeb9654fd4fee32b84410220edd3e
WhiteBelStudio <nikitinka221@gmail.com>
2026-09-18 21:15:49 +0300

    release: bump public API version to 1.2.13

--- server/public_api.py
+++ server/public_api.py
@@ -9,7 +9,7 @@ from pydantic import BaseModel
 import server.main as base
 
 app = base.app
-base.APP_VERSION = "1.2.12"
+base.APP_VERSION = "1.2.13"
 base.app.version = base.APP_VERSION
 init_db = base.init_db
 
