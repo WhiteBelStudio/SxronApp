e9193e8e2fcadbfa320c3553a1561c08ce1afada
WhiteBelStudio <nikitinka221@gmail.com>
2026-09-18 22:26:15 +0300

    Bump SXRON to 1.2.19

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ import "./styles/user-profile.css";
 import "./styles/theme.css";
 import "./styles/desktop-overrides.css";
 
-const APP_VERSION = "1.2.18";
+const APP_VERSION = "1.2.19";
 
 function syncStaticVersionLabels() {
   document.title = `SXRON Marketplace ${APP_VERSION}`;
