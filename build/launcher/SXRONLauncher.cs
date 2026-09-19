334d1d807ffc0872a09a306516e278270116f842
WhiteBelStudio <nikitinka221@gmail.com>
2026-09-19 21:38:40 +0300

    release: bump SXRON to 1.4.4

--- build/launcher/SXRONLauncher.cs
+++ build/launcher/SXRONLauncher.cs
@@ -124,7 +124,7 @@ internal static class SxronLauncher
         {
             using (var client = new WebClient())
             {
-                client.Headers[HttpRequestHeader.UserAgent] = "SXRON-Launcher/1.4.3";
+                client.Headers[HttpRequestHeader.UserAgent] = "SXRON-Launcher/1.4.4";
                 string json = client.DownloadString(
                     "https://api.github.com/repos/" + Owner + "/" + Repo + "/releases/latest"
                 );
@@ -161,7 +161,7 @@ internal static class SxronLauncher
 
         using (var client = new WebClient())
         {
-            client.Headers[HttpRequestHeader.UserAgent] = "SXRON-Launcher/1.4.3";
+            client.Headers[HttpRequestHeader.UserAgent] = "SXRON-Launcher/1.4.4";
             client.DownloadFile(url, installer);
         }
 
