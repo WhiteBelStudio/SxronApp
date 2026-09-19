21c71438e83d6dd698dc6b936afa976681aa61b1
WhiteBelStudio <nikitinka221@gmail.com>
2026-09-19 19:34:12 +0300

    SXRON 1.3.1 production hardening

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ import "./styles/user-profile.css";
 import "./styles/theme.css";
 import "./styles/desktop-overrides.css";
 
-const APP_VERSION = "1.3.0";
+const APP_VERSION = "1.3.1";
 
 function syncStaticVersionLabels() {
   document.title = `SXRON Marketplace ${APP_VERSION}`;
